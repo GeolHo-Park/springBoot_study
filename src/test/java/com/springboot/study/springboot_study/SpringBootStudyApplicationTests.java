1141e3d005fb1f4c38b75c1357038077e1eb9f1a
PARKGH <hoya54110@gmail.com>
2022-01-08 01:23:25 +0900

    20220108/am 01:23

---
+++ src/test/java/com/springboot/study/springboot_study/SpringBootStudyApplicationTests.java
@@ -0,0 +1,17 @@
+package com.springboot.study.springboot_study;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class SpringBootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+        int a = 1;
+        int b = 2;
+        Assertions.assertEquals(3, a+b);
+    }
+
+}
