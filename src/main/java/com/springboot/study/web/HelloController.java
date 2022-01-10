4efbfb1dedf643f03c371651133eb85f11a5eb59
PARKGH <hoya54110@gmail.com>
2022-01-10 22:51:55 +0900

    20220108/am 01:32

---
+++ src/main/java/com/springboot/study/web/HelloController.java
@@ -0,0 +1,15 @@
+package com.springboot.study.web;
+import com.springboot.study.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount)
+    {
+        return new HelloResponseDto(name, amount);
+    }
+}
---
+++ src/test/java/com/springboot/study/web/HelloControllerTest.java
@@ -0,0 +1,39 @@
+package com.springboot.study.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void HelloReturn() throws Exception{
+        String hello = "create";
+
+        mvc.perform(get("/create")).andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void HelloDtoReturn() throws Exception {
+        String name = "helloDto";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto").param("name", name).param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk()).andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+}
